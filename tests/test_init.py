59c1781851ddc4c79af6492ce26f69d30f27cd5b
drc38 <20024196+drc38@users.noreply.github.com>
2023-02-08 11:45:47 +1300

    Test update sensor naming

--- tests/test_init.py
+++ tests/test_init.py
@@ -39,7 +39,8 @@ async def test_setup_unload_and_reload_entry(hass, bypass_get_data):
     assert await async_setup_entry(hass, config_entry)
     await hass.async_block_till_done()
 
-    state = hass.states.get("sensor.test_energy")
+    # Note title is from Mock rather than PV_SYS_DATA as not using config flow
+    state = hass.states.get("sensor.mock_title_energy")
 
     assert state
     assert state.state == "1"
