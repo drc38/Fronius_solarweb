c69ca473b333026274ef69a873fece85ab98e06b
drc38 <20024196+drc38@users.noreply.github.com>
2023-09-01 14:37:59 +1200

    fix lingering timer issue

--- custom_components/solarweb/__init__.py
+++ custom_components/solarweb/__init__.py
@@ -148,6 +148,11 @@ class AggrDataUpdateCoordinator(DataUpdateCoordinator):
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
+    
+    # Not sure why needed but prevents lingering timers from DataUpdateCoordinator
+    for coord in hass.data[DOMAIN][entry.entry_id]:
+        await coord.async_shutdown()
+
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
     if unload_ok:
