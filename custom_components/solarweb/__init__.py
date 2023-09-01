61e50f8a9bb582803225680da5a1067d2933e98d
drc38 <20024196+drc38@users.noreply.github.com>
2023-09-01 14:41:38 +1200

    fix linting

--- custom_components/solarweb/__init__.py
+++ custom_components/solarweb/__init__.py
@@ -148,7 +148,7 @@ class AggrDataUpdateCoordinator(DataUpdateCoordinator):
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    
+
     # Not sure why needed but prevents lingering timers from DataUpdateCoordinator
     for coord in hass.data[DOMAIN][entry.entry_id]:
         await coord.async_shutdown()
