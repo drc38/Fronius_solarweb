f5549f4d36e840849f9d962cc8277f27c0ac1e9c
drc38 <20024196+drc38@users.noreply.github.com>
2023-11-03 14:52:40 +1300

    Fix ref

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@ from unittest.mock import patch
 
 import pytest
 import pytest_asyncio
-
 from fronius_solarweb.errors import NotAuthorizedException
 from fronius_solarweb.schema.pvsystem import PvSystemAggrDataV2
 from fronius_solarweb.schema.pvsystem import PvSystemFlowData
@@ -45,10 +44,10 @@ def skip_notifications_fixture():
 async def bypass_get_data_fixture():
     """Skip calls to get data from API."""
     with patch(
-        "fronius_solarweb.api.get_system_flow_data",
+        "fronius_solarweb.Fronius_Solarweb.api.get_system_flow_data",
         return_value=raw_flow_data,
     ), patch(
-        "fronius_solarweb.api.get_system_aggr_data_v2",
+        "fronius_solarweb.Fronius_Solarweb.api.get_system_aggr_data_v2",
         return_value=raw_aggr_data,
     ), patch(
         "fronius_solarweb.Fronius_Solarweb.get_pvsystem_meta_data",
@@ -66,10 +65,10 @@ def error_get_data_fixture():
         "fronius_solarweb.Fronius_Solarweb.get_pvsystem_meta_data",
         side_effect=NotAuthorizedException,
     ), patch(
-        "fronius_solarweb.api.get_system_flow_data",
+        "fronius_solarweb.Fronius_Solarweb.api.get_system_flow_data",
         side_effect=Exception,
     ), patch(
-        "fronius_solarweb.api.get_system_aggr_data_v2",
+        "fronius_solarweb.Fronius_Solarweb.api.get_system_aggr_data_v2",
         side_effect=Exception,
     ):
         yield
