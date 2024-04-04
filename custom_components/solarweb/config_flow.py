f513cf385bf6318c996e9523736ffba3af910940
drc38 <20024196+drc38@users.noreply.github.com>
2024-04-04 21:43:09 +1300

    improve flow

--- custom_components/solarweb/config_flow.py
+++ custom_components/solarweb/config_flow.py
@@ -45,10 +45,10 @@ class SolarWebFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         else:
             return await self._show_config_form(user_input)
 
-    async def _show_config_form(self, user_input):
+    async def _show_config_form(self, user_input, id="user"):
         """Show the configuration form to edit data."""
         return self.async_show_form(
-            step_id="user",
+            step_id=id,
             data_schema=vol.Schema(
                 {
                     vol.Required(CONF_PV_ID): str,
@@ -61,27 +61,19 @@ class SolarWebFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_reconfigure(self, user_input: dict[str, Any] | None = None):
         """Add reconfigure step to allow to reconfigure a config entry."""
+        entry = self.hass.config_entries.async_get_entry(self.context["entry_id"])
+        assert entry
+
         if user_input is not None:
             info = await self._validate_input(user_input)
             if info:
-                return self.async_update_entry(title=info["title"], data=user_input)
+                return self.async_update_reload_and_abort(
+                    entry, data=user_input, reason="reconfigure_successful"
+                )
             else:
                 self._errors["base"] = "auth"
 
-        return await self._show_reconfig_form(user_input)
-
-    async def _show_reconfig_form(self, user_input):
-        """Show the reconfiguration form to edit data."""
-        return self.async_show_form(
-            step_id="reconfigure",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_ACCESSKEY_ID): str,
-                    vol.Required(CONF_ACCESSKEY_VALUE): str,
-                }
-            ),
-            errors=self._errors,
-        )
+        return await self._show_config_form(user_input, id="reconfigure")
 
     async def _validate_input(self, data: dict[str, Any]) -> dict[str, Any]:
         """Validate the user input allows us to connect.
