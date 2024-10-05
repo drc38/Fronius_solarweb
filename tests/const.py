f977e30854509b60c7e9738c2c9f416ae82f28b6
drc38 <20024196+drc38@users.noreply.github.com>
2024-10-05 21:38:08 +1300

    changes to ruff uv (#330)
    * changes to ruff uv
    
    * fix name
    
    * fix filename
    
    * add pre-release
    
    * remove venv
    
    * update test

--- tests/const.py
+++ tests/const.py
@@ -37,7 +37,7 @@ PV_SYS_DATA = {
 PV_FLOW_DATA = {
     "pvSystemId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
     "deviceId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    "status": {"isOnline": True, "battMode": 0},
+    "status": {"isOnline": True, "battMode": "0"},
     "data": {
         "logDateTime": None,
         "channels": [
