10fa952747216cf8e683b43380a7a3fee8ab809b
drc38 <20024196+drc38@users.noreply.github.com>
2025-02-15 07:59:27 +1300

    fix expiration date logic (#419)
    * fix expiration date logic
    
    * fix other co-ord
    
    * fix ref

--- custom_components/solarweb/__init__.py
+++ custom_components/solarweb/__init__.py
@@ -177,6 +177,8 @@ class FlowDataUpdateCoordinator(DataUpdateCoordinator):
                     await self.hass.async_add_executor_job(
                         save_token, self.hass, self.api.jwt_data
                     )
+                    self.expires = self.api.jwt_data.get(TOKEN_EXPIRATION)
+                    _LOGGER.debug(f"Token new expiration: {self.expires}")
             data: PvSystemFlowData = await self.api.get_system_flow_data()
             _LOGGER.debug(f"Flow data polled: {data}")
 
@@ -218,6 +220,8 @@ class AggrDataUpdateCoordinator(DataUpdateCoordinator):
                     await self.hass.async_add_executor_job(
                         save_token, self.hass, self.api.jwt_data
                     )
+                    self.expires = self.api.jwt_data.get(TOKEN_EXPIRATION)
+                    _LOGGER.debug(f"Token new expiration: {self.expires}")
             data: PvSystemAggrDataV2 = await self.api.get_system_aggr_data_v2()
             _LOGGER.debug(f"Aggregated data polled: {data}")
 
